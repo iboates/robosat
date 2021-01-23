7994e7ca3ea29f533067c6d8544450ce2056a057
Isaac <iboates@gmail.com>
2021-01-23 19:37:39 +0100

    remove repo download link because that makes no sense

--- run.sh
+++ run.sh
@@ -21,8 +21,6 @@ conda create -n robosat python=3.8
 conda activate robosat
 
 # Clone the repo and install the deps, plus the ones that are missing for some reason
-git clone https://github.com/iboates/robosat.git
-cd robosat
 pip install -r requirements.in
 pip install torch
 pip install torchvision
