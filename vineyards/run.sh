84fcc8e06303736d357ab87e7076afe167402c5a
Isaac <iboates@gmail.com>
2021-04-04 19:48:44 +0200

    vineyards

--- vineyards/run.sh
+++ vineyards/run.sh
@@ -2,7 +2,10 @@
 set -e
 cd ..
 
-# Parse arguments
+weights=$(./rs weights --dataset vineyards/config/model-unet.toml)
+echo "Weights: $weights"
+sed -i "s/values = \[\]/values = $weights/" vineyards/config/model-unet.toml
+
 
 batch_size=$(cat vineyards/config/model-unet.toml | grep batch_size | cut -d'=' -f2 | xargs)
 echo "Training on $batch_size GPU(s)..."
